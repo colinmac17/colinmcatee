b4fbbbe29ce861c5e4f073afa61e2badfd5bb5cc
Colin McAtee <colin.g.mcatee@gmail.com>
2020-11-05 16:36:10 -0600

    adds mysql

--- source/views/tech.blade.php
+++ source/views/tech.blade.php
@@ -124,5 +124,16 @@
         </div>
       </div>
     </li>
+    <li class="col-span-1 flex shadow-sm rounded-md">
+      <div class="flex-shrink-0 flex items-center justify-center w-16 bg-orange-500 text-white text-sm leading-5 font-medium rounded-l-md">
+        <img alt="MySQL" src="https://pngimg.com/uploads/mysql/mysql_PNG23.png" class="h-12" />
+      </div>
+      <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+        <div class="flex-1 px-4 py-2 text-sm leading-5 truncate">
+          <a href="https://www.mysql.com/" target="_blank" class="text-gray-900 text-lg font-medium hover:text-gray-600 transition ease-in-out duration-150">MySQL</a>
+          <p class="text-gray-500 text-xs">Relational Database Management System</p>
+        </div>
+      </div>
+    </li>
   </ul>
 </div>
