4bd9e96524f68090b0b531ed6f02ab5c7df2df89
Colin McAtee <colin.g.mcatee@gmail.com>
2020-11-14 13:30:24 -0600

    Adds twitch

--- source/views/social.blade.php
+++ source/views/social.blade.php
@@ -5,11 +5,36 @@
                 <path d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
           </a>
-    <a target="_blank" href="https://twitter.com/colin_mcatee" class="text-gray-800 hover:text-gray-500">
+        <a target="_blank" href="https://twitter.com/colin_mcatee" class="text-gray-800 hover:text-gray-500">
             <span class="sr-only">Twitter</span>
             <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
             </svg>
+          </a>
+        <a target="_blank" href="https://www.twitch.tv/cmaaccc" class="text-gray-800 hover:text-gray-500">
+            <span class="sr-only">Twitch</span>
+{{--            <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">--}}
+{{--              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />--}}
+{{--            </svg>--}}
+            <svg class="h-6 w-6" fill="currentColor" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 2400 2800" style="enable-background:new 0 0 2400 2800;" xml:space="preserve">
+                <style type="text/css">
+                    .st0{fill:#FFFFFF;}
+                    /*.st1{fill:#9146FF;}*/
+                </style>
+                <title>Asset 2</title>
+                <g>
+                    <polygon class="st0" points="2200,1300 1800,1700 1400,1700 1050,2050 1050,1700 600,1700 600,200 2200,200 	"/>
+                    <g>
+                        <g id="Layer_1-2">
+                            <path class="st1" d="M500,0L0,500v1800h600v500l500-500h400l900-900V0H500z M2200,1300l-400,400h-400l-350,350v-350H600V200h1600
+                                V1300z"/>
+                            <rect x="1700" y="550" class="st1" width="200" height="600"/>
+                            <rect x="1150" y="550" class="st1" width="200" height="600"/>
+                        </g>
+                    </g>
+                </g>
+                </svg>
           </a>
           <a target="_blank" href="https://github.com/colinmac17" class="text-gray-800 hover:text-gray-500">
             <span class="sr-only">GitHub</span>
