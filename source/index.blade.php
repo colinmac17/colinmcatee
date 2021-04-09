cd752f024cfab206290731293959aa45d60a3867
Colin McAtee <colin.g.mcatee@gmail.com>
2021-04-09 17:05:01 -0500

    updates

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,11 +11,11 @@
             <p class="mt-2 max-w-md mx-auto text-lg text-gray-500 md:mt-3 md:max-w-3xl">
               I really love making stuff with code and learning new technologies.
             </p>
-            <span class="inline-flex rounded-md shadow-sm">
-              <a href="/blog" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150">
-                Read latest posts &rarr;
-              </a>
-            </span>
+{{--            <span class="inline-flex rounded-md shadow-sm">--}}
+{{--              <a href="/blog" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150">--}}
+{{--                Read latest posts &rarr;--}}
+{{--              </a>--}}
+{{--            </span>--}}
             @include('views.social')
           </div>
         </div>
