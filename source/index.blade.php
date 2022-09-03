08ba73e0d2002496cbda8f344f7d88b41409dc18
Colin McAtee <colin.g.mcatee@gmail.com>
2022-09-03 12:16:27 -0500

    meta image

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 ---
 title: "Colin McAtee"
 url: "https://colinmcatee.com"
-description: "Personal website and blog for ProductiveRecruit founder, and software engineer Colin McAtee"
+description: "Personal blog for ProductiveRecruit founder, and software engineer Colin McAtee"
 authorImage: "https://prodrecruit-assets.s3.amazonaws.com/site-assets/colin-headshot.webp"
 ogImage: "https://colinmcatee.com/assets/images/cm-meta-img2.png"
 ---
