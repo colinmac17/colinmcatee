f7f26816f0159b4157e0a921b8568cdd069089ce
Colin McAtee <colin.g.mcatee@gmail.com>
2022-09-09 06:04:58 -0500

    updates

--- source/index.blade.php
+++ source/index.blade.php
@@ -13,11 +13,12 @@ ogImage: "https://colinmcatee.com/assets/images/cm-meta.png"
     <div class="max-w-7xl space-y-8">
         <img src="/assets/images/CM.svg" class="rounded-full h-12 w-auto"/>
         <div class="space-y-1 text-gray-900">
-            <h1 class="text-2xl font-bold">Colin McAtee, founder of ProductiveRecruit</h1>
-            <p class="text-lg max-w-lg">I'm documenting my journey and what I learn building a B2B SaaS product.</p>
+            <h1 class="text-2xl font-bold">Colin McAtee</h1>
+            <p class="text-lg max-w-lg">Full-stack developer, and creator of ProductiveRecruit.</p>
             <ul class="list-disc py-4 ml-5 text-lg">
                 <li>I run a <a href="https://productiverecruit.com" target="_blank" class="text-[#0C63E7] underline">team-based college sports recruiting platform</a>, called ProductiveRecruit</li>
                 <li>I work as a Software Engineer for ActiveCampaign</li>
+                <li>Former Michigan Wolverine. Go blue!</li>
             </ul>
         </div>
         @include('_partials.footer')
